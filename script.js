949e25657fc7557e26b245df30924c15419e5d46
ZihadDomain <165703473+ZihadDomain@users.noreply.github.com>
2024-08-24 23:55:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var mark=54;
+
+ if(mark>100){
+    console.log("Mark is Invalid");
+}
+
+else if(mark<0){
+    console.log("Mark is Invalid");
+}
+
+else if(mark>=90){
+    console.log("You are pass in the Exam and Your Grade is 'Golden A+' ");
+}
+else if(mark>=80){
+    console.log("You are pass in the Exam and Your Grade is 'A+' ");
+}
+else if(mark>=70){
+    console.log("You are pass in the Exam and Your Grade is 'A+' ");
+}
+else if(mark>=60){
+    console.log("You are pass in the Exam and Your Grad is B");
+}
+else if(mark>=50){
+    console.log("You are pass in the Exam and Your Grad is C");
+}
+else if(mark>=40){
+    console.log("You are pass in the Exam and Your Grad is D");
+}
+
+else if(mark>=33){
+    console.log("You are pass in the Exam");
+}
+else{
+    console.log("You are fail in the exam")
+}
